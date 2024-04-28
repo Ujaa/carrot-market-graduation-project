f19d6eba53875fa40de33c9c45ea08cfed9635de
Yujin <yujinyll9148@gmail.com>
2024-04-29 01:35:41 +0900

    Feat: 상세 페이지 댓글 불러오기

---
+++ app/tweet/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { IPostReponse } from "@/model/reponses";
+import { headers } from "next/headers";
+import useSWR from "swr";
+
+interface PostDetailParams {
+  params: {
+    id: string;
+  };
+}
+
+export default async function PostDetail({ params: { id } }: PostDetailParams) {
+  const host = headers().get("host");
+  const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
+  const result = await fetch(`${protocol}://${host}/api/post/${id}`);
+  const json: IPostReponse = await result.json();
+
+  return (
+    <div>
+      <p>{json.post.content}</p>
+      <ul>
+        {json.post.comments.map((comment) => (
+          <li key={comment.id}>{comment.content}</li>
+        ))}
+      </ul>
+      <button>heart</button>
+    </div>
+  );
+}
